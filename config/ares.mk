f26ad60caa94a635396eb7e800ee390e060f18c6
priiii1808 <priyanshusinghal0818@gmail.com>
2024-06-06 15:01:41 +0300

    vendor: Bringup AresOS
    Change-Id: Ia46a0d089ddc7790f42cee10d3798c1846bb7322

---
+++ config/ares.mk
@@ -0,0 +1,15 @@
+# Extra packages
+PRODUCT_PACKAGES += \
+    GameSpace
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
+# Disable async MTE on system_server
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
